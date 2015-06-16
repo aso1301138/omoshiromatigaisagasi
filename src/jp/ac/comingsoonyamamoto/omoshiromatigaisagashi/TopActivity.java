c756b573204294df8674739ac94e7ba31afc3bf1
kanchikuryou <kanchikuryou@172.31.9.171>
2015-06-16 10:35:49 +0900

    あああ

--- src/jp/ac/comingsoonyamamoto/omoshiromatigaisagashi/TopActivity.java
+++ src/jp/ac/comingsoonyamamoto/omoshiromatigaisagashi/TopActivity.java
@@ -2,7 +2,10 @@ package jp.ac.comingsoonyamamoto.omoshiromatigaisagashi;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class TopActivity extends Activity {
 
@@ -10,21 +13,34 @@ public class TopActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
-    }
+    
     //遷移はだいたいこんな感じで！！！
     //buttonの取得
-  	//Button button = (Button)findViewById(R.id.Buttonの名前);
+  	Button button = (Button)findViewById(R.id.buttonStart);
+  	button.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO 自動生成されたメソッド・スタブ
+			Intent start = new Intent(TopActivity.this,LevelActivity.class);
+			startActivity(start);
+		}
+	});
+  	
+  	
   	//タップされたら
-  	//button.setOnClickListener(new View.OnClickListener() {
-  		//@Override
-  		//public void onClick(View v) {
-  			//
-  			//呼び出すためのインテント
+  	//button.setOnClickListener(new view.OnClickListener() {
+		
+		//@Override
+		//public void onClick(View v) {
+			// TODO 自動生成されたメソッド・スタブ
+			//呼び出すためのインテント
   			//Intent intent = new Intent(TopActivity.this,RankingActivity.class);
   			//startActivity(intent);
-  		//}
-  	//});
-
+		//}
+	//});
+  		
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
