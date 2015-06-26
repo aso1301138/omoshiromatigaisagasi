aa2acd2cc089c1aba6770bab6a3a4cd02185bfc7
shuwa <shuwa@172.31.11.159>
2015-06-26 12:21:06 +0900

    jfalksdjfalgkjl;asng

--- src/jp/ac/comingsoonyamamoto/omoshiromatigaisagashi/TopActivity.java
+++ src/jp/ac/comingsoonyamamoto/omoshiromatigaisagashi/TopActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class TopActivity extends Activity {
@@ -14,19 +13,27 @@ public class TopActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
+    }
+    //imagebuttonの取得
+    ImageButton ibs = (ImageButton)findViewById(R.id.iBstart);
     
-    //遷移はだいたいこんな感じで！！！
-    //buttonの取得
-  	ImageButton ibtn = (ImageButton)findViewById(R.id.iBstart);
-  	ibtn.setOnClickListener(new View.OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			// TODO 自動生成されたメソッド・スタブ
-			Intent start = new Intent(TopActivity.this,LevelActivity.class);
-			startActivity(start);
-		}
-	});
+    public void onClick(View v) {
+    	Intent intent = null;
+    	switch (v.getId()) {
+    	case (R.id.iBstart):
+    		intent = new Intent(TopActivity.this,LevelActivity.class);
+    		break;
+    	case (R.id.iBranking):
+    		intent = new Intent(TopActivity.this,RankingActivity.class);
+    		break;
+    	case (R.id.iBhelp):
+    		intent = new Intent(TopActivity.this,HelpActivity.class);
+    		break;
+    	case (R.id.iBoption):
+    		intent = new Intent(TopActivity.this,OptionActivity.class);
+    		break;
+    	}
+    	startActivity(intent);
     }
 
     @Override
