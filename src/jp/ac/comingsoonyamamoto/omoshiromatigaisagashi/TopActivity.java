e4fed86816a4ec527c459359bc79fd6c3b745729
shuwa <shuwa@172.31.11.82>
2015-07-01 14:01:12 +0900

    きゃ

--- src/jp/ac/comingsoonyamamoto/omoshiromatigaisagashi/TopActivity.java
+++ src/jp/ac/comingsoonyamamoto/omoshiromatigaisagashi/TopActivity.java
@@ -8,33 +8,58 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class TopActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
+        
+        //imagebuttonの取得
+        ImageButton ibs = (ImageButton)findViewById(R.id.iBstart);
+        ImageButton ibr = (ImageButton)findViewById(R.id.iBranking);
+        ImageButton ibh = (ImageButton)findViewById(R.id.iBhelp);
+        ImageButton ibo = (ImageButton)findViewById(R.id.iBoption);
+        
+        //startがタップされたら
+        ibs.setOnClickListener(new View.OnClickListener() {
+        	@Override
+        	public void onClick(View v){
+        		// TODO Auto-generated method stub
+        		Intent intent = new Intent(TopActivity.this,LevelActivity.class);
+        		startActivity(intent);
+        	}
+        });
+        
+        //rankingがタップされたら
+        ibr.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(TopActivity.this,RankingActivity.class);
+				startActivity(intent);
+			}
+		});
+        
+        //helpがタップされたら
+        ibh.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(TopActivity.this,HelpActivity.class);
+				startActivity(intent);
+			}
+		}); 
+        
+        //optionがタップされたら
+        ibo.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(TopActivity.this,OptionActivity.class);
+				startActivity(intent); 
+			}
+		}); 
     }
-    //imagebuttonの取得
-    ImageButton ibs = (ImageButton)findViewById(R.id.iBstart);
     
-    public void onClick(View v) {
-    	Intent intent = null;
-    	switch (v.getId()) {
-    	case (R.id.iBstart):
-    		intent = new Intent(TopActivity.this,LevelActivity.class);
-    		break;
-    	case (R.id.iBranking):
-    		intent = new Intent(TopActivity.this,RankingActivity.class);
-    		break;
-    	case (R.id.iBhelp):
-    		intent = new Intent(TopActivity.this,HelpActivity.class);
-    		break;
-    	case (R.id.iBoption):
-    		intent = new Intent(TopActivity.this,OptionActivity.class);
-    		break;
-    	}
-    	startActivity(intent);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
